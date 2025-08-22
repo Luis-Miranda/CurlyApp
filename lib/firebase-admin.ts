8f57d86be2d27b9bf164dde096170bd48288622c
Luis-Miranda <estevezlui758@gmail.com>
2025-08-22 01:10:57 -0600

    Firebase-admin.ts

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,16 +1,10 @@
-import { initializeApp, cert, getApps, getApp } from "firebase-admin/app"
-import { getAuth } from "firebase-admin/auth"
+import admin from "firebase-admin"
+import serviceAccount from "./firebase/firebase-service-account.json"
 
-const serviceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  })
 }
 
-const app = getApps().length === 0
-  ? initializeApp({
-      credential: cert(serviceAccount as any),
-    })
-  : getApp()
-
-export const adminAuth = getAuth(app)
+export const adminAuth = admin.auth()
